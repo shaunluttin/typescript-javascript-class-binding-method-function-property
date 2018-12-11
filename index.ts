9596b4dfbdfc4109e2615daededc8673b2c80a68
Shaun Luttin <shaunluttin@gmail.com>
2018-12-11 10:54:54 -0800

    Add small explanations

--- index.ts
+++ index.ts
@@ -35,19 +35,28 @@ class SomeClass {
         console.log('Press any key to see the output.')
 
         // why?
+        // someMethod acts like a function; binding depending on the context when its called
+        // someFunc is an arrow function; binding dependings on the context when its defined
         getStdin()
             .on('data', this.someMethod) // undefined
             .on('data', this.someFunc) // defined
 
         // why?
+        // someMethod act the same as before, but it is called from an arrow function
+        // someFunc acts the same as before
         getStdin()
             .on('data', () => this.someMethod()) // defined
             .on('data', () => this.someFunc()) // defined
 
         // why?
+        // we manually bind both in the defining context
         getStdin()
             .on('data', this.someMethod.bind(this)) // defined
             .on('data', this.someFunc.bind(this)) // defined
+
+        // see also
+        // https://stackoverflow.com/a/20279485/1108891
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
     }
 
     someMethod() {
